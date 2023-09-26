299e5ed12572e5e2f318f1863346dc0705be28da
Terry Gaul <trg01@trgaul.com>
2023-09-26 06:26:22 -0500

    Version 1.0 Red

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.red)
+            Text("Version 1.0")
+                .font(.largeTitle)
+                .fontWeight(.black)
         }
         .padding()
     }
