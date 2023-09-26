ed99b4e5704fb2b0961e22895eea277eeb839b9b
Terry Gaul <trg01@trgaul.com>
2023-09-26 06:53:50 -0500

    Version 2 - Green - Rectangle added

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
             Text("Version 2.0")
                 .font(.largeTitle)
                 .fontWeight(.black)
+            Rectangle()
         }
         .foregroundStyle(.green)
         .padding()
